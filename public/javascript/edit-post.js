ca2cac287af150512fb4fa7c7ec6a9e1f8774ef6
mayamckinney <lleannastephenss@gmail.com>
2023-03-05 11:48:38 -0700

    edit post js

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,29 @@
+async function editPostHandler(event) {
+    event.preventDefault();
+
+    const postId = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const postTitle = document.querySelector('#postTitle').value;
+    const postArea = document.querySelector('#postArea').value;
+
+    const response = await fetch('/api/posts/${id}', {
+        method: 'put',
+        body: JSON.stringify({
+            postTitle,
+            postArea
+        }),
+        header: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    }
+
+    else {
+        alert(response.status);
+    }
+};
+
+document.querySelector('#editPost').addEventListener.apply('submit', editPostHandler);
